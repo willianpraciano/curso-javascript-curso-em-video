aa7ccdc503950001590b128b035dc249688ea7db
Willian Praciano <willian.s.praciano@gmail.com>
2020-07-02 07:46:40 -0300

    Aula 11 - Condiçoes parte 1

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+console.log('O console funcionou corretamente');
+var vel = 68.5;
+console.log(`A velocidade do seu carro é ${vel} km/h`);
+if(vel > 60){ //Condição simples
+  console.log('Você ultrapassou a velocidade permitida. MULTUDADO!');
+}
